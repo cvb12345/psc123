76a09624a7059fae65c1f2a7cfb289720ef032c5
psc123 <qwew1234529@gmail.com>
2020-12-17 20:45:29 +0900

    십자선그리기

--- Chapter01/C0101/sketch.js
+++ Chapter01/C0101/sketch.js
@@ -3,9 +3,23 @@
 function setup()
 {
     createCanvas(800,600);
+
+    textSize(24);
 }
 
 function draw()
 {
     background(96,96,96);
+
+    stroke( 255,0,0 );
+
+    line(0, mouseY, windowWidth, mouseY);
+    textSize(18);
+    text("(" + mouseX + ")", 4,mouseY - 8);
+
+    stroke(0, 255, 0);
+
+    line(mouseX, 0, mouseX, windowHeight);
+    extSize(18);
+    text("(" + mouseY + ")", mouseX + 8,24);
 }
---
+++ Chapter01/C0102/sketch.js
@@ -0,0 +1,11 @@
+<reference path="../lib/p5.global-mode.d.ts"></reference>
+
+function setup()
+{
+    createCanvas(800,600);
+}
+
+function draw()
+{
+    background(96,96,96);
+}
